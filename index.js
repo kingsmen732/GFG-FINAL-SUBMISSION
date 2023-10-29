4f32e72e2c0db69ff861754292d8ea8320abe4ec
kingsmen732 <115350843+kingsmen732@users.noreply.github.com>
2023-10-29 14:57:13 +0530

    https://gfg-hackathon-host.pages.dev/
    final website "https://gfg-hackathon-host.pages.dev/"

---
+++ index.js
@@ -0,0 +1,55 @@
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({ 
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 500, fill: "forwards" });
+  
+  // for(const image of track.getElementsByClassName("image")) {
+  //   image.animate({
+  //     objectPosition: `${100 + nextPercentage}% center`
+  //   }, { duration: 500, fill: "forwards" });
+  // }
+}
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const google = 'https://www.allrecipes.com/search?q=+';
+const site = '';
+function submitted(event) {
+  event.preventDefault();
+  const url = google + site + '+' + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
+}
+f.addEventListener('submit', submitted);
